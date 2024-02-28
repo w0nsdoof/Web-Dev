86f77b07e7471302ff4b76e4c5778381b0e27744
Askar <w0nsdoof@gmail.com>
2024-02-28 20:04:34 +0600

    ...

--- lab4/src/app/product-list/product-list.component.ts
+++ lab4/src/app/product-list/product-list.component.ts
@@ -1,23 +1,45 @@
 import { Component } from '@angular/core';
-
-import { products } from '../products';
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
-  products = [...products];
+  products: Product[] = products;
+  searchInput: string = '';
 
-  share() {
-    window.alert('The product has been shared!');
+  get filteredProducts(): Product[] {
+    const searchTerm = this.searchInput.toLowerCase();
+    return this.products.filter(product => product.name.toLowerCase().includes(searchTerm));
+  }
+
+  share(productName: string, productLink: string) {
+    const shareMessage = `Check out this product: ${productName} - ${productLink}`;
+    const whatsappLink = `https://api.whatsapp.com/send?text=${encodeURIComponent(shareMessage)}`;
+    window.location.href = whatsappLink;
+  }
+
+  sharetg(productName: string, productLink: string) {
+    const shareMessage = `Check out this product: ${productName} - ${productLink}`;
+    const whatsappLink = `https://t.me/share/url?url=${encodeURIComponent(shareMessage)}`;
+    window.location.href = whatsappLink;
+  }
+
+  nextImage(product: Product) {
+    if (product.img.length > 1) {
+      product.currentImageIndex = (product.currentImageIndex + 1) % product.img.length;
+    }
+  }
+
+  prevImage(product: Product) {
+    if (product.img.length > 1) {
+      product.currentImageIndex =
+        (product.currentImageIndex - 1 + product.img.length) % product.img.length;
+    }
+  }
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
   }
 }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
